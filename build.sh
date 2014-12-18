23be9f066c60415b912c9e6f2dd1e3acc1051af5
CoolAcid <jakendall@gmail.com>
2014-12-18 12:57:12 -0500

    Add version to build zips

--- build.sh
+++ build.sh
@@ -4,25 +4,26 @@
 # Does each step and punches out some .zip files.
 
 SPWD=$(pwd)
+SVERSION=$(grep version package.json | cut -d\" -f4)
 
-npm install
-grunt
+#npm install
+#grunt
 
 # Compress the Win64 package
 cd $SPWD/build/TwitchTracker/win64/
-zip -r $SPWD/TwitchTracker-Win64.zip *
+zip -r $SPWD/TwitchTracker-$SVERSION-Win64.zip *
 
 # Compress the Win32 package
 cd $SPWD/build/TwitchTracker/win32/
-zip -r $SPWD/TwitchTracker-Win32.zip *
+zip -r $SPWD/TwitchTracker-$SVERSION-Win32.zip *
 
 # Compress the Linux64 package
 cd $SPWD/build/TwitchTracker/linux64/
-zip -r $SPWD/TwitchTracker-Linux64.zip *
+zip -r $SPWD/TwitchTracker-$SVERSION-Linux64.zip *
 
 # Compress the Linux32 package
 cd $SPWD/build/TwitchTracker/linux32/
-zip -r $SPWD/TwitchTracker-Linux32.zip *
+zip -r $SPWD/TwitchTracker-$SVERSION-Linux32.zip *
 
 cd $SPWD
 
